f564c7268960ea45b5dc6bd2759bf410a179b9e0
YuunsGit <yunus.emre.kepenek@outlook.com>
2023-12-14 00:43:47 +0300

    Hotfix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
       },
       boxShadow: {
         search:
-          "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;",
+          "rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px",
       },
       width: {
         animation: "512px",
